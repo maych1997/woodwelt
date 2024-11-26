51794ae7dec1c5169fffee24c251388e41c3433e
maych1997 <yousafarslan09@gmail.com>
2024-11-27 00:45:09 +0500

    Edit Profile

---
+++ src/components/Admin/Profile/EditProfile.js
@@ -0,0 +1,13 @@
+const EditProfile=()=>{
+    return(
+      <div className="profileContainer">
+      <div className="main-title">
+        <h3>Edit Profile </h3>
+      </div>
+      <div className="sub-container">
+      </div>
+      </div>
+    )
+  }
+
+  export default EditProfile;
